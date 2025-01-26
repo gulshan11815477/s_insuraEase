409376f9bb6aee00e470db689b5f6082d0d7d0a8
gulshan4700 <gulshan.kumar2godigit.com>
2025-01-26 23:33:48 +0530

    Adding basic structure of spring boot app

---
+++ src/main/java/com/insurEase/InsurEaseApplication.java
@@ -0,0 +1,13 @@
+package com.insurEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurEaseApplication.class, args);
+	}
+
+}
